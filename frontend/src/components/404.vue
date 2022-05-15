34e7fadd7dc9996f43d1963f76a9f9699e82a686
Jesus-pp <jesuspp@usal.es>
2022-05-16 01:15:22 +0200

    Home page finished

--- frontend/src/components/404.vue
+++ frontend/src/components/404.vue
@@ -7,7 +7,7 @@
 
 
 <script lang="ts">
-    import { defineComponent } from "@vue/runtime-core";
+    import { defineComponent } from "vue";
            
     export default defineComponent({
         name: '404Form',
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
-        <h2>Tipos de pérdidas:</h2>
-        
-        <div class="nav-item my-2" v-for="(loss, index) in lossTypes" :key="index">
-            <router-link :to="{ name: pathStatistics,  params: { type: loss.type } }" class="btn btn-primary" >{{loss.name}}</router-link>
+        <div class="container py-4">
+            <div class="row align-items-center">
+                <div class="d-flex flex-column">
+                    
+                    <!-- Presentación -->
+                    <h1 class="d-flex justify-content-center my-4"><b-icon-clipboard-data/> WarLoss</h1>
+                    <div class="d-flex justify-content-center mb-5">
+                        <img  src="../../public/armamento.jpg" alt="armamento" style="width:80%">
+                    </div>
+                    <h5 class="d-flex justify-content-center mb-5">
+                        Aplicación que muestra las pérdidas de guerra diarias según el tipo de vehículo o armamento. <br>
+                        Estos datos provienen de una base datos militar y podrán ser modificados, borrados o añadir más días. <br>
+                        El objetivo es tener la base de datos lo más actualizada posible para poder mantener a los usuario <br>
+                        al día de las pérdidas ocasionadas.
+
+                    </h5>
+                    <!-- Tipos de pérdidas -->
+                    <h2 class="d-flex justify-content-center mb-3">Tipos de pérdidas:</h2>                            
+
+                    <div class="nav-item my-2" v-for="(loss, index) in lossTypes" :key="index">
+                        <div class="d-grid mx-auto">
+                            <router-link :to="{ name: pathStatistics,  params: { type: loss.type } }" class="btn btn-primary py-3" ><b>{{loss.name}}</b></router-link>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
+
         
-        
-        
+
     </div>
 </template>
 
